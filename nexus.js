c4abf33f6b7aa2efc9ae3d617aab5681945afb14
trapped <giorgio.pellero@gmail.com>
2014-02-28 12:49:18 +0100

    Now in pure HTML and CSS

---
+++ nexus.js
@@ -0,0 +1,44 @@
+function reloadClient() {
+	document.getElementById("client").innerHTML = document.getElementById("client").innerHTML;
+}
+
+function getCookie(cname)
+{
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) 
+		{
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+		}
+	return "";
+}
+
+function getStyle() {
+	if(localStorage["style"] == "" || localStorage["style"] == undefined) {
+		return "light";
+	} else {
+		return localStorage["style"];
+	}
+}
+
+function setStyle(style) {
+	localStorage["style"] = style;
+	$("body").fadeOut(function() {
+		document.getElementById("stylesheet").setAttribute("href", "http://trapped.github.io/"+style+".css");
+		$(this).fadeIn();
+	});
+}
+
+function toggleStyle() {
+	if(getStyle() == "light") {
+		setStyle("dark");
+	} else {
+		setStyle("light");
+	}
+}
+
+window.addEventListener("load", function() {
+	setStyle(getStyle());
+	$('body').css('opacity', '0').fadeTo(200, 1);
+}, false);
